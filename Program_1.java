7e29d446561bfb780d199995ea96c7d78a2f1666
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-04 20:42:12 +0530

    Printing statement and comments in java

---
+++ Program_1.java
@@ -0,0 +1,18 @@
+// Printing statement and comments in Java
+
+public class Program_1
+{
+    public static void main(String [] args)
+    {
+
+        // This is a single line comment
+
+        /*  This
+        is a
+        multi-line
+        comment  */
+
+        System.out.println("Hello World");
+        System.out.println("I am Back");
+    }
+}
