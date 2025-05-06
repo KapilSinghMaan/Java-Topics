b6e2881c76df0a962d05cd5e3f54a3b287d6e61a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-06 23:15:02 +0530

    Substring in java

---
+++ Program_16.java
@@ -0,0 +1,34 @@
+// Substrings in java
+
+// .substring() = A method used to extract a portion of a string
+// .substring(start, end)
+
+import java.util.*;
+
+public class Program_16 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email: ");
+
+        email = scanner.nextLine();
+
+        if (email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println(username);
+            System.out.println(domain);
+        } else {
+            System.out.println("Emails must contain @");
+        }
+
+        scanner.close();
+    }
+}
