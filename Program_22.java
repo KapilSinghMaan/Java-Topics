e2035f1fff5a3dcbe17be54206a19898d8a79b5b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-07 22:53:23 +0530

    Logical Operators in java

---
+++ Program_22.java
@@ -0,0 +1,24 @@
+// Logical Operators in Java
+
+// && = AND
+// || = OR
+// ! = NOT
+
+public class Program_22 {
+    public static void main(String[] args) {
+
+
+        double temp = 35;
+        boolean isSunny = true;
+
+        if (temp <= 30 && temp >= 0 && isSunny) {
+            System.out.println("The weather is GOOD 😀");
+            System.out.println("It is SUNNY outside ☀");
+        } else if (temp <= 30 && temp >= 0 && !isSunny) {
+            System.out.println("The weather is GOOD 😀");
+            System.out.println("It is CLOUDY outside ☁");
+        } else if (temp > 30 || temp < 0) {
+            System.out.println("The weather is BAD 😩");
+        }
+    }
+}
