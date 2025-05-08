b45d02b3136f67d79b626be3bb7151953ef540bc
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-08 21:21:54 +0530

    break and continue in java

---
+++ Program_26.java
@@ -0,0 +1,16 @@
+// break = break out of a loop (STOP)
+// continue = skip current iteration of a loop (SKIP)
+
+public class Program_26 {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+                // continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
