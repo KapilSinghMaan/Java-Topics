f0793c6a8882bf9ab8b7489bdfe9870d1043070a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-09 21:47:49 +0530

    overloaded method/function in java

---
+++ Program_29.java
@@ -0,0 +1,27 @@
+// overloaded methods = methods that share the same name,
+// but different parameters
+// signature = name + parameters
+
+
+public class Program_29 {
+    public static void main(String[] args) {
+
+
+        String pizza = bakePizza("flat-bread", "mozzarella", "pepperoni");
+
+        System.out.println(pizza);
+
+    }
+
+    static String bakePizza(String bread) {
+        return bread + " pizza";
+    }
+
+    static String bakePizza(String bread, String cheese) {
+        return cheese + " " + bread + " pizza";
+    }
+
+    static String bakePizza(String bread, String cheese, String topping) {
+        return topping + " " + cheese + " " + bread + " pizza";
+    }
+}
