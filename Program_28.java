eecc26eea043a563df14395138426ac1b2b132d7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-09 21:47:15 +0530

    method/function in java

---
+++ Program_28.java
@@ -0,0 +1,35 @@
+// method =  a block of reusable code that is executed when called ()
+
+public class Program_28 {
+    public static void main(String[] args) {
+
+        happyBirthday("Spongebob", 30);
+    }
+
+    static void happyBirthday(String name, int age) {
+        System.out.println("Happy Birthday to you!");
+        System.out.printf("Happy Birthday dear %s!\n", name);
+        System.out.printf("You are %d years old!\n", age);
+        System.out.println("Happy Birthday to you!\n");
+    }
+
+    static double square(double number) {
+        return number * number;
+    }
+
+    static double cube(double number) {
+        return number * number * number;
+    }
+
+    static String getFullName(String first, String last) {
+        return first + " " + last;
+    }
+
+    static boolean ageCheck(int age) {
+        if (age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
