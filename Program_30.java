6305b2fc31f3673e300c47a12735df4a31e90a14
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-09 21:48:24 +0530

    Scope of variables in java

---
+++ Program_30.java
@@ -0,0 +1,23 @@
+// Scope of variables (Local and Class variables)
+
+public class Program_30 {
+    static int x = 3; // CLASS
+
+    public static void main(String[] args) {
+
+        int x = 1; // LOCAL
+
+        System.out.println(x);
+
+        doSomething();
+    }
+
+    static void doSomething() {
+
+        int x = 2; // LOCAL
+
+        System.out.println(x);
+    
+    }
+
+}
