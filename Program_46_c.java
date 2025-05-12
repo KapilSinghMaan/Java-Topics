51da09d2c5b4bf35faf1f81cfccbb42176be38bf
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-12 20:59:46 +0530

    Inheritance in Java (Part- 3)

---
+++ Program_46_c.java
@@ -0,0 +1,9 @@
+// Plant class (parent)
+
+public class Program_46_c extends Program_46_b{
+    
+    void photosynthesis()
+    {
+        System.out.println("The plants absorb sunlight");
+    }
+}
