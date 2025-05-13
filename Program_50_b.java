b452323d0464711ef1d5e52b7cf1a84862e2460c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-13 15:54:33 +0530

    abstract in java (Part-2)

---
+++ Program_50_b.java
@@ -0,0 +1,13 @@
+// Abstract class
+
+// Shape class (parent)
+
+public abstract class Program_50_b {
+    
+    abstract double area();   // Abstract method
+
+    void display()
+    {
+        System.out.println("This is a shape");
+    }
+}
