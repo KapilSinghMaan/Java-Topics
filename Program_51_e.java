ad3c92b13f977486b93e0718a8a2da304fc45ca4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-14 23:00:06 +0530

    Interface in java (Part-5)

---
+++ Program_51_e.java
@@ -0,0 +1,10 @@
+// Hawk class (child)
+
+public class Program_51_e implements Program_51_c {
+
+    @Override
+    public void hunt() {
+        System.out.println("The Hawk is hunting");
+    }
+
+}
