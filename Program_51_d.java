4808a6d844c6fe2208be88b07848b4ca56454461
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-14 22:59:48 +0530

    Interface in java (Part-4)

---
+++ Program_51_d.java
@@ -0,0 +1,9 @@
+// rabbit class (child)
+
+public class Program_51_d implements Program_51_b {
+
+    @Override
+    public void flee() {
+        System.out.println("The rabbit is running");
+    }
+}
