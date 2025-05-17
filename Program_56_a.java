13298ec89299ff87ff77a6ddb8f1a7e58a08c96f
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-17 22:31:25 +0530

    Composition in java (Part-1)

---
+++ Program_56_a.java
@@ -0,0 +1,17 @@
+// Composition = Represents a "part-of" relationship between objects.
+//                            For example, an Engine is "part of" a Car.
+//                            Allows complex objects to be constructed from smaller objects.
+
+public class Program_56_a {
+    public static void main(String[] args) {
+
+
+        Program_56_b car = new Program_56_b("Corvette", 2025, "V8");
+
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.engine.type);
+
+        car.start();
+    }
+}
