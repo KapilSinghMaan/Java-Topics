f2d29ea997b802cd24572e02255bd545e8723722
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-17 22:32:07 +0530

    Composition in java (Part-3)

---
+++ Program_56_c.java
@@ -0,0 +1,11 @@
+public class Program_56_c {
+    String type;
+
+    Program_56_c(String type){
+        this.type = type;
+    }
+
+    void start(){
+        System.out.println("You start the " + this.type + " engine");
+    }
+}
