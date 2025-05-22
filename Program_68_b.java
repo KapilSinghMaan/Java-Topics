4abe80711a76134f8bdf5ccef3966e02882c6103
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-22 21:43:30 +0530

    Generics in java(Part-2)

---
+++ Program_68_b.java
@@ -0,0 +1,19 @@
+// Product class
+
+public class Program_68_b<T, U> {
+    T item;
+    U price;
+
+    Program_68_b(T item, U price) {
+        this.item = item;
+        this.price = price;
+    }
+
+    public T getItem() {
+        return this.item;
+    }
+
+    public U getPrice() {
+        return this.price;
+    }
+}
