3b0be86a99420bc48b2ad7172a5ce40f9898f15d
KapilSinghMaan <kapilmaan369@gmail.com>
2025-05-23 22:06:02 +0530

    Enums in Java(Part-2)

---
+++ Program_70_b.java
@@ -0,0 +1,16 @@
+// Day enum
+
+public enum Program_70_b {
+    SUNDAY(1), MONDAY(2), TUESDAY(3), WEDNESDAY(4),
+    THURSDAY(5), FRIDAY(6), SATURDAY(7);
+
+    private final int dayNumber;
+
+    Program_70_b(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+    public int getDayNumber() {
+        return this.dayNumber;
+    }
+}
